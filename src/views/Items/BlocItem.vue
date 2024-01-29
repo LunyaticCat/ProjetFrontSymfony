5086a7c296c52c6d3f5f4fcc9824e1b5cc354dbe
Gatien <depeyre.gatien@gmail.com>
2024-01-29 10:50:35 +0100

    delete item ajouté

--- src/views/Items/BlocItem.vue
+++ src/views/Items/BlocItem.vue
@@ -12,19 +12,34 @@ const carac = defineProps<{
   item: Item
 }>();
 
-
+function deleteItem(){
+  fetch('https://webinfo.iutmontp.univ-montp2.fr/~campsa/ProjetApiSymfony/public/api/items/'+carac.item.id, {
+            method: "DELETE"})
+    .then(reponsehttp => {
+      if(reponsehttp.status!==204){
+        alert("La suppression n'a pas marchée");
+      } else{
+        alert("Item supprimé");
+        carac.item.id = -1;
+      }
+   
+    })
+}
 
 </script>
 
 <template>
 
-    <div id="item">
+    <div id="item" v-if="item.id!=-1">
         <div>
            {{ item?.id }}
             
         </div>
         <div>
           <a @click="router.push('/itemhierarchy/'+item?.id)">{{ item?.nameItem }}</a>
+        </div>
+        <div>
+          <button v-on:click="deleteItem()">Delete item</button>
         </div>
       </div>
 
@@ -41,6 +56,9 @@ const carac = defineProps<{
     padding: 10px 0 10px 0;
     margin: 5px;
     text-align: center;
+}
+div#item div{
+  padding-top: 10px;
 }
 
 a{
